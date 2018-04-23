d8f108a958c63f3578f8a62af0b5047a42fbbc9e
Purple Myst <PurpleMyst@users.noreply.github.com>
2018-04-23 15:03:56 +0200

    Use String over CString

--- src/completion_code.rs
+++ src/completion_code.rs
@@ -1,15 +1,16 @@
 //! A module that contains the [`CompletionCode`](enum.CompletionCode.html).
-use std::ffi::CString;
+
+use std::borrow::Cow;
 
 /// Enum that represents Tcl's usually-ints completion codes in a more rustic manner.
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub enum CompletionCode {
+pub enum CompletionCode<'interp> {
     /// Represents that everything went fine.
     Ok,
 
     /// Represents that there was an error.
     /// Contains the error message left as the result.
-    Error(CString),
+    Error(Cow<'interp, str>),
 
     /// Represents that the last line executed was a result.
     Return,
@@ -21,12 +22,12 @@ pub enum CompletionCode {
     Continue,
 }
 
-impl CompletionCode {
+impl<'interp> CompletionCode<'interp> {
     /// Panics if the `self` is a [`CompletionCode::Error`], else does nothing.
     #[inline]
     pub fn panic_if_error(self) {
         if let CompletionCode::Error(msg) = self {
-            panic!("{:?}", msg);
+            panic!("{}", msg);
         }
     }
 }
--- src/interp.rs
+++ src/interp.rs
@@ -1,10 +1,10 @@
 //! A module that holds the [`TclInterp`](struct.TclInterp.html) struct.
 use super::{completion_code::CompletionCode, rusty_tcl_sys};
 
-use std::convert::AsRef;
-use std::ffi::CStr;
-use std::os::raw::{c_int, c_uint};
-use std::ptr::NonNull;
+use std::{borrow::Cow,
+          ffi::{CStr, CString},
+          os::raw::{c_int, c_uint},
+          ptr::NonNull};
 
 /// Interpreter struct that holds the Tcl interpreter itself.
 // TODO: mark this not thread safe
@@ -27,15 +27,15 @@ impl TclInterp {
     ///
     /// # Errors
     /// This function returns an [`Err`] value with a [`CompletionCode::Error`] if [`Tcl_AppInit`](https://tcl.tk/man/tcl/TclLib/AppInit.htm) returns an error completion code.
-    pub fn new() -> Result<Self, CompletionCode> {
+    pub fn new() -> Result<Self, CompletionCode<'static>> {
         super::init();
         // TODO: Use `Option::ok_or_else` here.
         let interp_ptr = NonNull::new(unsafe { rusty_tcl_sys::Tcl_CreateInterp() }).unwrap();
 
         let mut this = Self { interp_ptr };
 
-        if let err @ CompletionCode::Error(_) = this.app_init() {
-            return Err(err);
+        if let CompletionCode::Error(err_msg) = this.app_init() {
+            return Err(CompletionCode::Error(Cow::from(err_msg.into_owned())));
         }
 
         Ok(this)
@@ -46,8 +46,14 @@ impl TclInterp {
     }
 
     /// Fetches the interpreter's internal string result.
-    pub fn get_string_result<'a>(&self) -> &'a CStr {
-        unsafe { CStr::from_ptr(rusty_tcl_sys::Tcl_GetStringResult(self.interp_ptr.as_ptr())) }
+    ///
+    /// # Panics
+    /// This function panics if the interpreter's internal string result is invalid UTF-8. This
+    /// should never happen.
+    pub fn get_string_result(&self) -> &str {
+        let c_result: &CStr =
+            unsafe { CStr::from_ptr(rusty_tcl_sys::Tcl_GetStringResult(self.interp_ptr.as_ptr())) };
+        c_result.to_str().unwrap()
     }
 
     /// Make this interpreter safe.
@@ -76,7 +82,7 @@ impl TclInterp {
     fn completioncode_from_int(&self, raw_completion_code: c_int) -> CompletionCode {
         match raw_completion_code as c_uint {
             rusty_tcl_sys::TCL_OK => CompletionCode::Ok,
-            rusty_tcl_sys::TCL_ERROR => CompletionCode::Error(self.get_string_result().to_owned()),
+            rusty_tcl_sys::TCL_ERROR => CompletionCode::Error(Cow::from(self.get_string_result())),
             rusty_tcl_sys::TCL_RETURN => CompletionCode::Return,
             rusty_tcl_sys::TCL_BREAK => CompletionCode::Break,
             rusty_tcl_sys::TCL_CONTINUE => CompletionCode::Continue,
@@ -87,39 +93,47 @@ impl TclInterp {
 
     /// Evaluates a piece of Tcl code.
     ///
+    /// # Panics
+    /// This function panics if `code` contains NULL bytes.
+    ///
     /// # Notes
     /// This just returns a [`CompletionCode`], to get the code's result you need to use
     /// [`TclInterp::get_string_result`].
-    pub fn eval(&mut self, code: impl AsRef<CStr>) -> CompletionCode {
+    pub fn eval(&mut self, code: impl Into<Vec<u8>>) -> CompletionCode {
+        let c_code = CString::new(code).unwrap();
+
         let raw_completion_code =
-            unsafe { rusty_tcl_sys::Tcl_Eval(self.interp_ptr.as_ptr(), code.as_ref().as_ptr()) };
+            unsafe { rusty_tcl_sys::Tcl_Eval(self.interp_ptr.as_ptr(), c_code.as_ptr()) };
 
         self.completioncode_from_int(raw_completion_code)
     }
 
     /// Sets a variable with the given `name` to the given `value`.
     ///
+    /// # Panics
+    /// This function panics if `name` or `value` contain NUL bytes.
+    ///
     /// # Notes
     /// This returns the value that `name` was set to, which may differ from `value` due to
     /// tracing.
     pub fn set_var<'a>(
         &mut self,
-        name: impl AsRef<CStr>,
-        value: impl AsRef<CStr>,
+        name: impl Into<Vec<u8>>,
+        value: impl Into<Vec<u8>>,
     ) -> Result<&'a CStr, CompletionCode> {
         let flags: c_int = rusty_tcl_sys::TCL_LEAVE_ERR_MSG as c_int;
 
         let result_ptr = unsafe {
             rusty_tcl_sys::Tcl_SetVar(
                 self.interp_ptr.as_ptr(),
-                name.as_ref().as_ptr(),
-                value.as_ref().as_ptr(),
+                CString::new(name).unwrap().as_ptr(),
+                CString::new(value).unwrap().as_ptr(),
                 flags,
             )
         };
 
         if result_ptr.is_null() {
-            Err(CompletionCode::Error(self.get_string_result().to_owned()))
+            Err(CompletionCode::Error(Cow::from(self.get_string_result())))
         } else {
             Ok(unsafe { CStr::from_ptr(result_ptr) })
         }
@@ -128,14 +142,7 @@ impl TclInterp {
 
 #[cfg(test)]
 mod tests {
-    use std::ffi::CString;
-    use {CompletionCode, TclInterp};
-
-    macro_rules! cstr {
-        ($s:expr) => {
-            CString::new($s).unwrap()
-        };
-    }
+    use TclInterp;
 
     #[test]
     fn it_works() {
@@ -143,19 +150,15 @@ mod tests {
 
         macro_rules! tcl_assert_eq {
             ($cc:expr, $expected:expr) => {{
-                let cc = $cc;
-                if let CompletionCode::Error(msg) = cc {
-                    panic!("{}", msg.into_string().unwrap());
-                }
-
-                assert_eq!(interp.get_string_result(), cstr!($expected).as_ref());
+                $cc.panic_if_error();
+                assert_eq!(interp.get_string_result(), $expected);
             }};
         };
 
-        tcl_assert_eq!(interp.eval(cstr!("expr {2 + 2}")), "4");
+        tcl_assert_eq!(interp.eval("expr {2 + 2}"), "4");
 
-        assert!(interp.set_var(cstr!("x"), cstr!("5")).is_ok());
-        tcl_assert_eq!(interp.eval(cstr!("return $x")), "5");
+        assert!(interp.set_var("x", "5").is_ok());
+        tcl_assert_eq!(interp.eval("return $x"), "5");
 
         interp.make_safe().panic_if_error();
         assert!(interp.is_safe());
